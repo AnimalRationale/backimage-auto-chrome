39b12f58f2c42e43e00974d9a8f9491812cb26aa
AnimalRationale <wojciech.mardyla@gmail.com>
2015-02-22 23:04:57 +0100

    Detecting declared string in page URL.

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(function() {
             {
                 conditions: [
                     new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { urlContains: 'trello' },
+                        pageUrl: { urlContains: 'trello' }
                     })
                 ],
                 actions: [ new chrome.declarativeContent.ShowPageAction() ]
